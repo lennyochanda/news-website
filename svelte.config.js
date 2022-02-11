41e325d84600114da7c0a14d36a28f7390101883
dak-384 <dakkofficial@gmail.com>
2022-02-11 12:15:14 +0300

    SCSS config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+			scss: {
+				// Ensures Sass variables are always available inside component <style> blocks
+				prependData: `@import 'src/lib/assets/scss/_vars.scss';`
+			}
+		}),
 	kit: {
 		adapter: adapter()
 	}
