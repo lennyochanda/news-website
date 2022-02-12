acf1f7b20262cc53785448af3d7a78141c92d8fb
dak-384 <dakkofficial@gmail.com>
2022-02-12 13:37:08 +0300

    Add User store

--- src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import {createClient} from '@supabase/supabase-js'
-import config from 'config'
-
-const SUPABASE_URL = config.get('SUPABASE_URL')
-const SUPABASE_ANON_KEY = config.get('SUPABASE_ANON_KEY')
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY) 
