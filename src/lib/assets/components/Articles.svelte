696ef78e55a0dfb5eb47e99479b14da3950ef6c2
dak-384 <dakkofficial@gmail.com>
2022-02-19 20:51:37 +0300

    Macro layout mockup

---
+++ src/lib/assets/components/Articles.svelte
@@ -0,0 +1,20 @@
+<div class="cards">
+	<article>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta quaerat natus facilis labore, esse suscipit dolorum, ratione, tempora officia nihil omnis? Repudiandae laboriosam, amet! Rem tenetur ipsam doloremque ipsa neque.
+	</article>
+	<article>
+		Voluptatibus ducimus totam, quam quasi autem voluptatem eligendi doloribus sapiente deleniti optio non alias rerum, omnis saepe facere impedit aliquam quos eveniet, sed dolorem et sit aspernatur, quae cum magnam?
+	</article>
+	<article>
+		Perferendis quisquam officia doloribus debitis, odit earum quae quis repudiandae necessitatibus eligendi impedit dignissimos veritatis voluptatum libero. Dolor eveniet in suscipit amet, ad magni ullam, illo quia a autem reiciendis?
+	</article>
+	<article>
+		Hic praesentium quos, impedit facilis laudantium provident ea pariatur amet, sint repellendus, dicta alias ratione molestias sed excepturi assumenda quod maxime accusamus enim laboriosam! Saepe deleniti, nihil pariatur quam soluta?
+	</article>
+	<article>
+		Officiis, voluptas accusantium ducimus cum. Atque natus incidunt voluptates magni eveniet voluptas inventore vitae ut, reiciendis tenetur cum tempore nostrum, soluta nihil commodi enim repellat aliquid alias odit voluptate, delectus.
+	</article>
+	<article>
+		Vitae est eaque modi mollitia itaque praesentium libero delectus nisi debitis ipsum eius ducimus, minus vero voluptatibus, quis aliquam, quaerat id. Laudantium dolor provident cumque, minus fugit eum molestiae praesentium?
+	</article>
+	</div>
