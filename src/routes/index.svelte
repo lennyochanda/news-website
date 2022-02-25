57c146439a09f9ea2358add3b5ae3eaa25927bc0
dak-384 <dakkofficial@gmail.com>
2022-02-25 20:58:58 +0300

    Add video and callout section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,15 @@
 <script>
 	import Hero from '$lib/assets/components/Hero.svelte'
+	import Callout from '$lib/assets/components/Callout.svelte'
 	import Articles from '$lib/assets/components/Articles.svelte'
 	import Aside from '$lib/assets/components/Aside.svelte'
 </script>
 
 <main>
 	<div class="container">
-	<Hero />	
-	<Articles />
-	<Aside />
+		<Hero />
+		<Callout />
+		<Articles />
+		<Aside />
 	</div>
 </main>
